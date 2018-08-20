18a8d3ca4ffed8b1ac975df69ba173f012b0bc56
CuiJiaLei <369191739@qq.com>
2018-08-20 18:08:47 +0800

    small demo project

--- smalldemo/src/main/java/com/example/smalldemo/Thread/AsynchronizationThread.java
+++ smalldemo/src/main/java/com/example/smalldemo/Thread/AsynchronizationThread.java
@@ -8,9 +8,9 @@ package com.example.smalldemo.Thread;
 public class AsynchronizationThread {
     public static void main(String[] args){
         System.out.println("开始做蛋糕");
-        // (2) 为了建立RealData的实体，启动新的线程
-
+        // TODO: 准备工作 通知开始
         new Thread(){
+            // TODO: 另起线程实现异步
             public void run() {
                 //在匿名内部类中使用count、future、c。
                 for (int i=1;i<=5;i++){
@@ -24,7 +24,6 @@ public class AsynchronizationThread {
                 System.out.println("做完蛋糕,通知客户过来拿吧");
             }
         }.start();
-
 //        new Thread(new Runnable() {
 //            @Override
 //            public void run() {
@@ -35,7 +34,7 @@ public class AsynchronizationThread {
 //                System.out.println("做完蛋糕,通知客户过来拿吧");
 //            }
 //        }).start();
-//
+        // TODO: 先通知到生产者已完成
         System.out.println("你先回去等吧");
 
     }
