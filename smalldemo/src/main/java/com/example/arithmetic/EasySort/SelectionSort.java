656e68ac3e52c5208a12f9ae3b9bfd20784a154d
CuiJiaLei <369191739@qq.com>
2018-11-21 17:56:48 +0800

    冒泡排序，选择排序

---
+++ smalldemo/src/main/java/com/example/arithmetic/EasySort/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.example.arithmetic.EasySort;
+
+import java.util.Arrays;
+
+/**
+ * @Author:cuijialei
+ * @Date: 2018/11/21
+ * @Describe 选择排序
+ */
+public class SelectionSort {
+    public static void main(String []arg){
+        int[] ints = {3,21,12,99,44,23,44};
+
+        int minIndex=0;
+        for(int i=0 ; i<ints.length;i++){
+            minIndex=i;//无序区的最小数据数组下标
+            for(int j=i+1;j<ints.length;j++){
+                if(ints[minIndex]>ints[j]){
+                    minIndex = j;
+                }
+            }
+            int temp=ints[i];
+            ints[i]=ints[minIndex];
+            ints[minIndex]=temp;
+        }
+        System.out.println(Arrays.toString(ints));
+    }
+}
