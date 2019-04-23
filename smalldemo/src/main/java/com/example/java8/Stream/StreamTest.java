11f63226bdcf6e2676188ddbd744e54812a5fd25
CuiJiaLei <369191739@qq.com>
2019-04-23 18:14:05 +0800

    README.MD 文件修改

--- smalldemo/src/main/java/com/example/java8/Stream/StreamTest.java
+++ smalldemo/src/main/java/com/example/java8/Stream/StreamTest.java
@@ -1,9 +1,47 @@
 package com.example.java8.Stream;
 
+import com.example.java8.Stream.model.PersonModel;
+
+import java.util.List;
+
 /**
  * @Author:cuijialei
  * @Date: 2019/4/23
  * @Describe
  */
 public class StreamTest {
+
+    private static List<PersonModel> list = null;
+
+    static {
+        PersonModel zhang = new PersonModel("zhangsan",18,true,170.5);
+        PersonModel lisi = new PersonModel("lisi",19,true,182.1);
+        PersonModel wangwu = new PersonModel("wangwu",22,true,175.0);
+        PersonModel liufang = new PersonModel("liufang",18,false,166.5);
+        PersonModel wangqi = new PersonModel("wangqi",19,false,172.5);
+        PersonModel zhangsan = new PersonModel("zhangsan",15,false,162.5);
+
+    }
+
+    public static List<PersonModel> getData(){
+        return list;
+    }
+
+    /**
+     * filter
+     * 1、遍历数据并检查其中的元素时使用。
+     * 2、filter接受一个函数作为参数，该函数用Lambda表达式表示。
+     */
+
+    public static void fiterSex(){
+        List<PersonModel> data = getData();
+
+
+    }
+
+
+    private void print(List<PersonModel> list){
+        list.forEach(personModel -> System.out.println(personModel));
+    }
+
 }
---
+++ smalldemo/src/main/java/com/example/java8/Stream/model/PersonModel.java
@@ -0,0 +1,65 @@
+package com.example.java8.Stream.model;
+
+/**
+ * @Author:cuijialei
+ * @Date: 2019/4/23
+ * @Describe
+ */
+public class PersonModel {
+    private String name;
+    private int age;
+    private boolean man;
+    private double height;
+
+    public PersonModel() {
+    }
+
+    public PersonModel(String name, int age, boolean man, double height) {
+        this.name = name;
+        this.age = age;
+        this.man = man;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonModel{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", man=" + man +
+                ", height=" + height +
+                '}';
+    }
+}
