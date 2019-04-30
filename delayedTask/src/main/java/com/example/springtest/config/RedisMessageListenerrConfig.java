77a7cc3d0b012f5f81d0a6089d67898995211294
CuiJiaLei <369191739@qq.com>
2019-04-30 18:06:10 +0800

    秒级定时任务：redis方案2 （测试出现问题，键空间通知无法返回）

--- delayedTask/src/main/java/com/example/springtest/config/RedisMessageListenerrConfig.java
+++ delayedTask/src/main/java/com/example/springtest/config/RedisMessageListenerrConfig.java
@@ -3,10 +3,15 @@ package com.example.springtest.config;
 
 
 import com.example.springtest.service.impl.RedisMessageListener;
+import com.example.springtest.util.DateUtils;
 import org.redisson.Redisson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -17,6 +22,8 @@ import java.util.Objects;
 
 @Configuration
 public class RedisMessageListenerrConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisMessageListenerrConfig.class);
 
     private final RedisTemplate<Object, Object> redisTemplate;
     private final RedisMessageListener messageListener;
@@ -35,7 +42,7 @@ public class RedisMessageListenerrConfig {
         // 设置监听的Topic __keyevent@0__:expired 表示监听db0的过期事件
         ChannelTopic channelTopic = new ChannelTopic("__keyevent@0__:expired");
         // 设置监听器
-        container.addMessageListener(messageListener, channelTopic);
+        container.addMessageListener((message, pattern) -> LOGGER.info("监听Topic：{}，key:{}已过期，当前时间{}", new String(pattern), message.toString(),DateUtils.getStringTime(System.currentTimeMillis())), channelTopic);
         return container;
     }
 }
