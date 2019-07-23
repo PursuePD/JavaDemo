70a0bc9a9b701ae3da18a76ee59235fb3076550d
CuiJiaLei <369191739@qq.com>
2019-07-23 11:04:15 +0800

    多线程

--- smalldemo/src/main/java/com/example/smalldemo/Thread/mypool/MyThreadPool.java
+++ smalldemo/src/main/java/com/example/smalldemo/Thread/mypool/MyThreadPool.java
@@ -1,8 +1,7 @@
-package com.example.mypool;
+package com.example.smalldemo.Thread.mypool;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ThreadFactory;
 
 /**
  * @Author:cuijialei
